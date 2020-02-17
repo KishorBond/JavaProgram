f143c3632663f8a411b6a8187c0a16c0cdfb9bcd
KishorBond <45976400+KishorBond@users.noreply.github.com>
2020-02-17 19:27:08 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package Example_01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myFirstNumber = (10*3) + (2+3);
+		int mySecondNumber = 20;
+		System.out.println(myFirstNumber);
+		int myTotal = myFirstNumber + mySecondNumber;
+		System.out.println(myTotal);
+		int myNewNumber = 1000 - myTotal;
+		System.out.println(myNewNumber);
+
+	}
+}
